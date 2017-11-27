9599d08b98c87283569dfb0f200f35881f6e5a93
guillaume <4388440+gu-ma@users.noreply.github.com>
2017-11-27 22:31:02 +0100

    Modified example 01 for colors

---
+++ examples/04_01/sketch.js
@@ -0,0 +1,83 @@
+// Based on the code P_2_0_02.pde from
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
+
+// Global var
+
+function setup() {
+  // Canvas setup
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("p5Container");
+  // Detect screen density (retina)
+  var density = displayDensity();
+  pixelDensity(density);
+  // Init var
+}
+
+function draw() {
+
+  let offset = 40;
+
+  noStroke();
+  fillHsluv(0, 0, 90);
+  rect(0, 0, width/2, height);
+  fillHsluv(0, 0, 85);
+  rect((width/4 - width/6), (height/2 - height/8) , width/3, height/4);
+  fillHsluv(0, 0, 95);
+  rect((width/4 - width/6) + offset, (height/2 - height/8) + offset, width/3, height/4);
+  fillHsluv(0, 0, 0);  
+
+  let txt = 'A color scheme with low contrast';
+  let txtWidth = textWidth(txt);
+  text(txt, width/4 - txtWidth/2, (height/2 - height/8) - 50);
+
+  fillHsluv(0, 0, 0);
+  rect(width/2, 0, width, height);
+  fillHsluv(0, 0, 50);
+  rect(width/2 + (width/4 - width/6), (height/2 - height/8) ,width/3, height/4);
+  fillHsluv(0, 0, 100);
+  rect(width/2 + (width/4 - width/6) + offset, (height/2 - height/8) + offset, width/3, height/4);
+
+  txt = 'A color scheme with high contrast';
+  txtWidth = textWidth(txt);
+  text(txt, width/2 + width/4 - txtWidth/2, (height/2 - height/8) - 50);
+
+}
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveThumb(650, 350);
+}
+
+// Color functions
+
+function fillHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+function strokeHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+// Tools
+
+// resize canvas when the window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight, false);
+}
+
+// Int conversion
+function toInt(value) {
+  return ~~value;
+}
+
+// Timestamp
+function timestamp() {
+  return Date.now();
+}
+
+// Thumb
+function saveThumb(w, h) {
+  let img = get(width/2 - w/2, height/2 - h/2, w, h);
+  save(img, 'thumb.jpg');
+}
