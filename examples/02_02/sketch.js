43587799fc085ac05bda297afca406930457a321
guillaume <4388440+gu-ma@users.noreply.github.com>
2017-12-01 12:16:24 +0100

    fixed the agent example (02_02)

--- examples/02_02/sketch.js
+++ examples/02_02/sketch.js
@@ -7,6 +7,7 @@ var NORTH, EAST, SOUTH, WEST;
 var posX, posY, posXcross, posYcross;
 var direction, angleCount, angle, stepSize, minLength;
 var dWeight, dStroke, drawMode;
+var canvas, backgroundGrey;
 
 function setup() {
   // Canvas setup
@@ -15,10 +16,9 @@ function setup() {
   // Detect screen density (retina)
   var density = displayDensity();
   pixelDensity(density);
-  // Color Mode
-  colorMode(HSB, 360, 100, 100, 100);
   // Init var
   // some of the var are initialised in gui.js
+  backgroundGrey = 50;
   NORTH = 0;
   EAST = 1;
   SOUTH = 2;
@@ -27,7 +27,7 @@ function setup() {
   direction = SOUTH;
   angleCount = 7;    //// 1 - ...
   angle = getRandomAngle(direction);
-  stepSize = 3;
+  stepSize = 10;
   minLength = 10;
   // width and brightness of the stroke depend on line length
   dWeight = 50;
@@ -39,61 +39,59 @@ function setup() {
   posY = 5;
   posXcross = posX;
   posYcross = posY;
+  background(backgroundGrey);
 }
 
 function draw() {
 
   smooth();
+  stepSize = map(mouseX, 1, width, 1, 20);
 
-  for (let i=0; i<=mouseX; i++) {
+  // ------ draw dot at current position ------
+  strokeWeight(1);
+  stroke(200);
+  point(posX, posY);
 
-    // ------ draw dot at current position ------
-    strokeWeight(1);
-    stroke(180);
-    point(posX, posY);
+  // ------ make step ------
+  posX += cos(radians(angle)) * stepSize;
+  posY += sin(radians(angle)) * stepSize;
 
-    // ------ make step ------
-    posX += cos(radians(angle)) * stepSize;
-    posY += sin(radians(angle)) * stepSize;
+  // ------ check if agent is near one of the display borders ------
+  let reachedBorder = false;
 
-    // ------ check if agent is near one of the display borders ------
-    let reachedBorder = false;
-
-    if (posY <= 5) {
-      direction = SOUTH;
-      reachedBorder = true;
-    } 
-    else if (posX >= width-5) {
-      direction = WEST;
-      reachedBorder = true;
-    }
-    else if (posY >= height-5) {
-      direction = NORTH;
-      reachedBorder = true;
-    }
-    else if (posX <= 5) {
-      direction = EAST;
-      reachedBorder = true;
-    }
-
-    // ------ if agent is crossing his path or border was reached ------
-    // let pixelColors = get(toInt(posX),toInt(posY));
-    // text(pixelColors, width / 2, 50);
-
-    if (reachedBorder) {
-      angle = getRandomAngle(direction);
-      let distance = dist(posX, posY, posXcross, posYcross);
-      if (distance >= minLength) {
-        strokeWeight(distance/dWeight);
-        if (drawMode == 1) stroke(0);
-        if (drawMode == 2) stroke(52, 100, distance/dStroke);
-        if (drawMode == 3) stroke(192, 100, 64, distance/dStroke);
-        line(posX, posY, posXcross, posYcross);
-      }
-      posXcross = posX;
-      posYcross = posY;
-    }
+  if (posY <= 5) {
+    direction = SOUTH;
+    reachedBorder = true;
+  } 
+  else if (posX >= width-5) {
+    direction = WEST;
+    reachedBorder = true;
   }
+  else if (posY >= height-5) {
+    direction = NORTH;
+    reachedBorder = true;
+  }
+  else if (posX <= 5) {
+    direction = EAST;
+    reachedBorder = true;
+  }
+
+  // ------ if agent is crossing his path or border was reached ------
+  let pixelColors = get(toInt(posX),toInt(posY));
+  if ( brightness(color(pixelColors)) != brightness(color(backgroundGrey)) || reachedBorder) {
+    angle = getRandomAngle(direction);
+    let distance = dist(posX, posY, posXcross, posYcross);
+    if (distance >= minLength) {
+      strokeWeight(distance/dWeight);
+      if (drawMode == 1) stroke(0);
+      if (drawMode == 2) stroke(52, 100, distance/dStroke);
+      if (drawMode == 3) stroke(192, 100, 64, distance/dStroke);
+      line(posX, posY, posXcross, posYcross);
+    }
+    posXcross = posX;
+    posYcross = posY;
+  }
+
 }
 
 function getRandomAngle(theDirection) {
